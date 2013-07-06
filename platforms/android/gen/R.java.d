fbcdd3cd211d2de58ca5aa98a0e00665671e5508
Oren Nis <orennis@gmail.com>
2013-07-06 12:36:27 +0300

    First commint
    Barcode scanning

---
+++ platforms/android/gen/R.java.d
@@ -0,0 +1,14 @@
+G:\MobileSDK\WIP\platforms\android\gen\com\oren\WipApp\R.java \
+ : G:\MobileSDK\WIP\platforms\android\res\drawable\icon.png \
+G:\MobileSDK\WIP\platforms\android\res\drawable-hdpi\icon.png \
+G:\MobileSDK\WIP\platforms\android\res\drawable-hdpi\ic_launcher.png \
+G:\MobileSDK\WIP\platforms\android\res\drawable-ldpi\icon.png \
+G:\MobileSDK\WIP\platforms\android\res\drawable-ldpi\ic_launcher.png \
+G:\MobileSDK\WIP\platforms\android\res\drawable-mdpi\icon.png \
+G:\MobileSDK\WIP\platforms\android\res\drawable-mdpi\ic_launcher.png \
+G:\MobileSDK\WIP\platforms\android\res\drawable-xhdpi\icon.png \
+G:\MobileSDK\WIP\platforms\android\res\drawable-xhdpi\ic_launcher.png \
+G:\MobileSDK\WIP\platforms\android\res\layout\main.xml \
+G:\MobileSDK\WIP\platforms\android\res\values\strings.xml \
+G:\MobileSDK\WIP\platforms\android\res\xml\config.xml \
+G:\MobileSDK\WIP\platforms\android\bin\AndroidManifest.xml \
