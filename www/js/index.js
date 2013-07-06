450df17598d6ce940e783d6a109a5544bd414830
Oren Nis <orennis@gmail.com>
2013-07-06 12:57:51 +0300

    ..

--- www/js/index.js
+++ www/js/index.js
@@ -18,26 +18,26 @@
  */
 var app = {
     // Application Constructor
-    initialize: function() {
+    initialize: function () {
         this.bindEvents();
     },
     // Bind Event Listeners
     //
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
+    bindEvents: function () {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
+    onDeviceReady: function () {
         app.receivedEvent('deviceready');
         $("#ScannedBarcode").val("ready to scan");
     },
     // Update DOM on a Received Event
-    receivedEvent: function(id) {
+    receivedEvent: function (id) {
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
@@ -47,14 +47,17 @@ var app = {
 
         console.log('Received Event: ' + id);
     },
-    scan: function() {
-	var scanner = cordova.require("cordova/plugin/BarcodeScanner");
+    scan: function () {
+        alert("Scan button clicked");
+        var scanner = cordova.require("cordova/plugin/BarcodeScanner");
 
-	scanner.scan(function (result) {
-        $("#ScannedBarcode").val(result.text);
-		},
+        scanner.scan(function (result) {
+            alert("Scanning...");
+            $("#ScannedBarcode").val(result.text);
+            alert("Scanned!");
+        },
 		function (error) {
-			alert("Scanning failed: " + error);
+		    alert("Scanning failed: " + error);
 		}
 	);
     }
